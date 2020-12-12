556adea20102c46e878b2c79a28e51f2f18d4203
Rajmohan <rajmohan6268@gmail.com>
2020-12-12 10:46:56 +0530

    initial commit 😍

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+        segoe: ['Segoe UI', 'Arial', 'sans-serif'],
+        raleway: ['Raleway', 'Arial', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
+}
